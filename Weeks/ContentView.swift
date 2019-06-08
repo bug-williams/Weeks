b548cb18e7ca4da5d6bcb317c5819664f44b4fed
Zachary Williams <zachwilliams.services@gmail.com>
2019-06-07 22:37:14 -0400

    Initial Commit

---
+++ Weeks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Weeks
+//
+//  Created by Zachary Williams on 6/7/19.
+//  Copyright © 2019 Zachary Williams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
