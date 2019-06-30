79f8400f30a1acbc4df2278a2d8fa8abafe83fdf
Zachary Williams <zachwilliams.services@gmail.com>
2019-06-29 20:45:49 -0400

    Created project, added basic habit cell layout.

--- Weeks/ContentView.swift
+++ Weeks/ContentView.swift
@@ -10,7 +10,46 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World!")
+        HStack {
+            NavigationView {
+                List {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.blue)
+                        VStack {
+                            HStack(alignment: .center) {
+                                Text("Habit Title")
+                                    .font(.title)
+                                    .bold()
+                                    .color(Color.white)
+                                Spacer()
+                                Image(systemName: "hourglass")
+                                    .font(.subheadline)
+                                    .colorInvert()
+                                Text("2 days")
+                                    .font(.subheadline)
+                                    .bold()
+                                    .color(Color.white)
+                            }
+                            HStack(alignment: .bottom) {
+                                ForEach(0 ..< 5) { item in
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .fill(Color.white)
+                                        .frame(width: 10, height: 30)
+                                }
+                                Text("5 credits")
+                                    .font(.subheadline)
+                                    .bold()
+                                    .color(Color.white)
+                                Spacer()
+                            }
+                        }
+                            .padding(EdgeInsets(top: 15, leading: 20, bottom: 15, trailing: 20))
+                    }
+                }
+                    .navigationBarTitle(Text("Habits"))
+            }
+        }
     }
 }
 
